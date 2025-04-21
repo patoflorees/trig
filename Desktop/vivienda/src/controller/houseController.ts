8a1f825429e441fa1b04df03d2acd21a31b25169
patoflorees <patoflores.garcia@icloud.com>
2025-04-21 08:02:14 -0600

    Delete Desktop/vivienda directory

--- Desktop/vivienda/src/controller/houseController.ts
+++
@@ -1,9 +0,0 @@
-export class HouseController {
-
-
-    async getHousesByType(type:string,minPrice:number,maxPrice:number){
-        const dbService = new FakeService();
-        const dbService.getHouses{type,minPrice,maxPrice}
-        return houses
-    }
-}
--- Desktop/vivienda/src/db/types/dbService.ts
+++
@@ -1,6 +0,0 @@
-import {House} from "../../types/db/house";
-
-interface IDBService {
-    getHouses{type:string,minPrice:number, maxPrice:number}:Promise<House[]>
-    getHousesById{idText:number}; Promise<House>
-}
